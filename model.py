65d340d1d9350c3122c93df78c6323abd0f3433b
bear-boy <1223376137@qq.com>
2024-03-24 15:09:19 +0800

    Create DPCRN Model

---
+++ model.py
@@ -0,0 +1,142 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from modules import DPRNN
+import utils
+
+class Stft(nn.Module):
+    def __init__(self, frame_len, frame_hop):
+        super(Stft, self).__init__()
+        self.eps = torch.finfo(torch.float32).eps
+        self.frame_len = frame_len
+        self.frame_hop = frame_hop
+
+    def forward(self, x):
+        if len(x.shape) != 2:
+            print("x must be in [B, T]")
+        y = torch.stft(x, n_fft=self.frame_len, hop_length=self.frame_hop,
+                       win_length=self.frame_len, return_complex=True, center=False)
+        r = y.real
+        i = y.imag
+        # mag = torch.clamp(r ** 2 + i ** 2, self.eps) ** 0.5
+        # phase = torch.atan2(i + self.eps, r + self.eps)
+        # r shape: BxFxT, i shape: BxFxT
+        return r, i
+
+
+class DPCRN(nn.Module):
+    def __init__(self, encoder_in_channel, encoder_channel_size, encoder_kernel_size, encoder_stride_size, encoder_padding,
+                       decoder_in_channel, decoder_channel_size, decoder_kernel_size, decoder_stride_size,
+                       rnn_type, input_size, hidden_size,
+                       frame_len, frame_shift):
+        super(DPCRN, self).__init__()
+        self.encoder_channel_size = encoder_channel_size
+        self.encoder_kernel_size = encoder_kernel_size
+        self.encoder_stride_size = encoder_stride_size
+        self.encoder_padding = encoder_padding
+        self.decoder_channel_size = decoder_channel_size
+        self.decoder_kernel_size = decoder_kernel_size
+        self.decoder_stride_size = decoder_stride_size
+
+        self.stft = Stft(frame_len=frame_len, frame_hop=frame_shift)
+        self.encoder = Encoder(encoder_in_channel, self.encoder_channel_size,
+                               self.encoder_kernel_size, self.encoder_stride_size, self.encoder_padding)
+        self.decoder = Decoder(decoder_in_channel, self.decoder_channel_size,
+                               self.decoder_kernel_size, self.decoder_stride_size)
+        self.dprnn = DPRNN(rnn_type, input_size=input_size, hidden_size=hidden_size)
+
+    def forward(self, x):
+        re, im = self.stft(x)
+        inputs = torch.stack([re,im],dim=1).permute(0,1,3,2)     # B x C x T x F
+        x, skips = self.encoder(inputs)
+
+        x = x.permute(0,1,3,2)                  # B x C x F x T
+        x = self.dprnn(x)
+        x = x.permute(0,1,3,2)                  # B x C x T x F
+
+        y = self.decoder(x, skips)
+        return y
+
+class Encoder(nn.Module):
+    def __init__(self, in_channel_size, channel_size, kernel_size, stride_size, padding):
+        super(Encoder, self).__init__()
+        self.channel_size = channel_size
+        self.kernel_size = kernel_size
+        self.stride_size = stride_size
+        self.padding = padding
+
+        self.conv = nn.ModuleList()
+        self.norm = nn.ModuleList()
+        in_chan = in_channel_size
+        for i in range(len(channel_size)):
+            self.conv.append(nn.Conv2d(in_channels=in_chan,out_channels=channel_size[i],
+                                       kernel_size=kernel_size[i], stride=stride_size[i]))
+            self.norm.append(nn.BatchNorm2d(channel_size[i]))
+            in_chan = channel_size[i]
+        self.prelu = nn.PReLU()
+
+    def forward(self, x):
+        # x shape: B x C x T x F
+        skips = []
+        for i, (layer, norm) in enumerate(zip(self.conv, self.norm)):
+            x = F.pad(x, pad=self.padding[i])
+            x = layer(x)
+            x = self.prelu(norm(x))
+            skips.append(x)
+        return x, skips
+
+class Decoder(nn.Module):
+    def __init__(self, in_channel_size, channel_size, kernel_size, stride_size):
+        super(Decoder, self).__init__()
+        self.channel_size = channel_size
+        self.kernel_size = kernel_size
+        self.stride_size = stride_size
+
+        self.conv = nn.ModuleList()
+        self.norm = nn.ModuleList()
+        in_chan = in_channel_size
+        for i in range(len(channel_size)):
+            if i == 3:
+                self.conv.append(nn.ConvTranspose2d(in_channels=in_chan, out_channels=channel_size[i],
+                                                    kernel_size=kernel_size[i], stride=stride_size[i],
+                                                    padding=[0, 1], output_padding=[0, 1]))
+            else:
+                self.conv.append(nn.ConvTranspose2d(in_channels=in_chan, out_channels=channel_size[i],
+                                                    kernel_size=kernel_size[i], stride=stride_size[i],
+                                                    padding=[0,1]))
+            self.norm.append(nn.BatchNorm2d(channel_size[i]))
+            in_chan = channel_size[i] * 2
+        self.prelu = nn.PReLU()
+
+    def forward(self, x, skips):
+        # x shape: B x C x T x F
+        for i, (layer, norm, skip) in enumerate(zip(self.conv, self.norm, reversed(skips))):
+            x = torch.cat([x,skip], dim=1)
+            x = layer(x)[:,:,:-1,:]
+            x = self.prelu(norm(x))
+        return x
+
+def test():
+    hps = utils.get_hparams()
+    model = DPCRN(encoder_in_channel=hps.train.encoder_in_channel,
+                  encoder_channel_size=hps.train.encoder_channel_size,
+                  encoder_kernel_size=hps.train.encoder_kernel_size,
+                  encoder_stride_size=hps.train.encoder_stride_size,
+                  encoder_padding=hps.train.encoder_padding,
+                  decoder_in_channel=hps.train.decoder_in_channel,
+                  decoder_channel_size=hps.train.decoder_channel_size,
+                  decoder_kernel_size=hps.train.decoder_kernel_size,
+                  decoder_stride_size=hps.train.decoder_stride_size,
+                  rnn_type=hps.train.dprnn_rnn_type,
+                  input_size=hps.train.dprnn_input_size,
+                  hidden_size=hps.train.dprnn_hidden_size,
+                  frame_len=hps.train.frame_len,
+                  frame_shift=hps.train.frame_shift)
+    model.eval()
+    batch_size = 16
+    x = torch.randn((batch_size, 16000*5)) # 5s inputs
+    y = model(x)
+    return y
+
+if __name__ == "__main__":
+    test()
---
+++ modules.py
@@ -0,0 +1,106 @@
+import torch
+import torch.nn as nn
+
+EPS = 1e-8
+
+class SingleRNN(nn.Module):
+    """
+    Container module for a single RNN layer.
+
+    args:
+        rnn_type: string, select from 'RNN', 'LSTM' and 'GRU'.
+        input_size: int, dimension of the input feature. The input should have shape
+                    (batch, seq_len, input_size).
+        hidden_size: int, dimension of the hidden state.
+        dropout: float, dropout ratio. Default is 0.
+        bidirectional: bool, whether the RNN layers are bidirectional. Default is False.
+    """
+
+    def __init__(self, rnn_type, input_size, hidden_size, dropout=0, bidirectional=False):
+        super(SingleRNN, self).__init__()
+
+        self.rnn_type = rnn_type
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_direction = int(bidirectional) + 1
+
+        self.rnn = getattr(nn, rnn_type)(input_size, hidden_size, 1, dropout=dropout, batch_first=True,
+                                         bidirectional=bidirectional)
+
+        # linear projection layer
+        self.proj = nn.Linear(hidden_size * self.num_direction, input_size)
+
+    def forward(self, input):
+        # input shape: batch, seq, dim
+        #input = input.to(device)
+        output = input
+        rnn_output, _ = self.rnn(output)
+        rnn_output = self.proj(rnn_output.contiguous().view(-1, rnn_output.shape[2])).view(output.shape)
+        return rnn_output
+
+# dual-path RNN
+class DPRNN(nn.Module):
+    """
+    Deep duaL-path RNN.
+
+    args:
+        rnn_type: string, select from 'RNN', 'LSTM' and 'GRU'.
+        input_size: int, dimension of the input feature. The input should have shape
+                    (batch, seq_len, input_size).
+        hidden_size: int, dimension of the hidden state.
+        output_size: int, dimension of the output size.
+        dropout: float, dropout ratio. Default is 0.
+        num_layers: int, number of stacked RNN layers. Default is 1.
+        bidirectional: bool, whether the RNN layers are bidirectional. Default is False.
+    """
+
+    def __init__(self, rnn_type, input_size, hidden_size,
+                 dropout=0, num_layers=1, bidirectional=False):
+        super(DPRNN, self).__init__()
+
+        self.input_size = input_size
+        # self.output_size = output_size
+        self.hidden_size = hidden_size
+
+        # dual-path RNN
+        self.row_rnn = nn.ModuleList([])
+        self.col_rnn = nn.ModuleList([])
+        self.row_norm = nn.ModuleList([])
+        self.col_norm = nn.ModuleList([])
+        for i in range(num_layers):
+            self.row_rnn.append(SingleRNN(rnn_type, input_size, hidden_size, dropout,
+                                          bidirectional=True))  # intra-segment RNN is always noncausal
+            self.col_rnn.append(SingleRNN(rnn_type, input_size, hidden_size, dropout, bidirectional=bidirectional))
+            self.row_norm.append(nn.GroupNorm(1, input_size, eps=1e-8))
+            # default is to use noncausal LayerNorm for inter-chunk RNN. For causal setting change it to causal normalization techniques accordingly.
+            self.col_norm.append(nn.GroupNorm(1, input_size, eps=1e-8))
+
+        # no output layer in DPCRN
+        # self.output = nn.Sequential(nn.PReLU(),
+        #                             nn.Conv2d(input_size, output_size, 1)
+        #                             )
+
+    def forward(self, input):
+        # input shape: batch, N, dim1, dim2
+        # apply RNN on dim1 first and then dim2
+        # output shape: B, output_size, dim1, dim2
+        #input = input.to(device)
+        batch_size, _, dim1, dim2 = input.shape
+        output = input
+        for i in range(len(self.row_rnn)):
+            row_input = output.permute(0, 3, 2, 1).contiguous().view(batch_size * dim2, dim1, -1)  # B*dim2, dim1, N
+            row_output = self.row_rnn[i](row_input)  # B*dim2, dim1, H
+            row_output = row_output.view(batch_size, dim2, dim1, -1).permute(0, 3, 2,
+                                                                             1).contiguous()  # B, N, dim1, dim2
+            row_output = self.row_norm[i](row_output)
+            output = output + row_output
+
+            col_input = output.permute(0, 2, 3, 1).contiguous().view(batch_size * dim1, dim2, -1)  # B*dim1, dim2, N
+            col_output = self.col_rnn[i](col_input)  # B*dim1, dim2, H
+            col_output = col_output.view(batch_size, dim1, dim2, -1).permute(0, 3, 1,
+                                                                             2).contiguous()  # B, N, dim1, dim2
+            col_output = self.col_norm[i](col_output)
+            output = output + col_output
+
+        # output = self.output(output) # B, output_size, dim1, dim2
+        return output
